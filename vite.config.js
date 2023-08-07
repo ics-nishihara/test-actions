18f3d99fa6c9a3623143896c17059b89e2ce4608
ics-nishihara <nishihara@ics-web.jp>
2023-08-07 14:19:55 +0900

    環境変数の設定

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-export default {
-  root: "./src",
-  build: {
-    outDir: "../dist",
-  },
-  base: "/test-actions/",
-};
+import { defineConfig, loadEnv } from 'vite'
+
+const config = defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    root: "./src",
+    build: {
+      outDir: "../dist",
+    },
+    base: env.BASE_PATH,
+  }
+
+});
+
+export default config
